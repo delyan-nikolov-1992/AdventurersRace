4e94584b5418e1e82cfdd79423e55561c117350e
Delyan Nikolov <delyan.nikolov.1992@gmail.com>
2014-09-29 14:32:39 -0500

    Changes made by Delyan Nikolov (delyan.nikolov.1992@gmail.com).

--- AdventurersRace/scripts/app/contacts.js
+++ AdventurersRace/scripts/app/contacts.js
@@ -8,30 +8,54 @@ app.contacts = (function () {
     'use strict';
     
     function show(e) {
+        var phoneNumbers = [];
+        
         function onSuccess(contacts) {
-            //debugger;
-            console.log(contacts);
+            var i,
+                j,
+                contactsLength,
+                phonesLength;
             
+            for (i = 0, contactsLength = contacts.length; i < contactsLength; i++) {
+                var phones = contacts[i].phoneNumbers;
+                
+                for (j = 0, phonesLength = phones.length; j < phonesLength; j++) {
+                    //console.log(phones[j].value);
+                    var currentPhone = phones[j].value;
+                    phoneNumbers.push(currentPhone);
+                }
+            }
         }
 
-        function onError(contactError) {
-            alert('onError!');
+        function onError() {
+            alert('An error has occured by getting the contacts');
         }
         
         function getContacts() {
-             //find all contacts with 'Bob' in any name field
-            //debugger;
-            var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+            var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.phoneNumbers];
             var options = new ContactFindOptions();
-            options.filter = "t";
             options.multiple = true;
             navigator.contacts.find(fields, onSuccess, onError, options);
         }
         
-       // debugger;
         getContacts();
-        //console.log(navigator);
-        //console.log(navigator.contacts);
+        getUserFromDb();
+        
+        function getUserFromDb() {
+            var filter = new Everlive.Query();
+            filter.where().isin('Phone', phoneNumbers);
+
+            var data = everlive.data('Users');
+            
+            data.get(filter)
+                .then(function(data) {
+                    //alert(JSON.stringify(data));
+                    console.log(data);
+                },
+                      function(error) {
+                          alert(JSON.stringify(error));
+                      });
+        }
         
         var data = {
             data: [{
