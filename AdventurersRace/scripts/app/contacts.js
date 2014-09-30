8caffed682c79efbe74335a600173f16199ea7c6
Delyan Nikolov <delyan.nikolov.1992@gmail.com>
2014-09-30 03:30:28 -0500

    Changes made by Delyan Nikolov (delyan.nikolov.1992@gmail.com).

--- AdventurersRace/scripts/app/contacts.js
+++ AdventurersRace/scripts/app/contacts.js
@@ -46,35 +46,39 @@ app.contacts = (function () {
             var el = new Everlive('HAp0eHbnKuAuxQsa');
             var filter = new Everlive.Query();
             filter.where().isin('Phone', phoneNumbers);
+            filter.orderDesc('Points');
 
             var data = el.data('Users');
             
             data.get(filter)
                 .then(function(data) {
                     //alert(JSON.stringify(data));
-                    console.log(data);
-                    console.log(data.result[0].Username);
+                    //console.log(data);
+                    var vm = kendo.observable(data);
+                    //app.contacts.vm = vm;
+                    //console.log(vm);
+                    kendo.bind($("#listContacts"), vm);
+                    //console.log(data.result[0].Username);
                 },
                       function(error) {
                           alert(JSON.stringify(error));
                       });
         }
         
-        var data = {
-            data: [{
-                        Points: 1,
-                        Username: 'Bob'
-                    }, {
-                        Points: 2,
-                        Username: 'Mary'
-                    }
-            ]
-        };
-        
-        var vm = kendo.observable(data);
-        app.contacts.vm = vm;
-        console.log(vm);
-        //kendo.bind(e.view.element, vm);
+        //var data = {
+        //    data: [{
+        //                Points: 1,
+        //                Username: 'Bob'
+        //            }, {
+        //                Points: 2,
+        //                Username: 'Mary'
+        //            }
+        //    ]
+        //};
+        //var vm = kendo.observable(data);
+        //app.contacts.vm = vm;
+        //console.log(vm);
+        //kendo.bind($("#listContacts"), vm);
     }
 
     return {
